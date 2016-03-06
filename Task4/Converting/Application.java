d261c8d268c1ac191b6a56ee7986ea76a4f1c9f6
OleksandrFedyay <oleksandr.fedyay@gmail.com>
2016-03-06 21:28:37 +0200

    Add Task4

---
+++ Task4/Converting/Application.java
@@ -0,0 +1,13 @@
+/**
+ * Created by FedYAY on 06.03.2016.
+ */
+public class Application {
+    public static void main (String[] args){
+
+        Converting toFahrenheit = new Converting();
+        toFahrenheit.convertToFahrenheit(0);
+
+        Converting toCelsius = new Converting();
+        toCelsius.convertToCelsius(0);
+    }
+}
