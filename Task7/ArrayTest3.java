f76267cabf2511232a19861e5c0e9f63d788c148
OleksandrFedyay <oleksandr.fedyay@gmail.com>
2016-03-21 20:06:09 +0200

    Add Task7

---
+++ Task7/ArrayTest3.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayTest3 {
+
+    static int[] array;
+    static final int NUMBER_OF_ELEMENTS = 3;//Declare constant variable instead of using Magic Number 3
+    static final int SHIFTING_VALUE_OF_COUNTER = 1;//Declare constant variable instead of using Magic Number 1
+
+    public static void main(String[] args) {
+
+        array = new int[NUMBER_OF_ELEMENTS];
+        final int LAST_INDEX = array.length - 1;
+        int lowerValue;
+        int highestValue;
+        Scanner input = new Scanner(System.in);
+
+        for (int count = 0; count < array.length; count++) {
+
+            System.out.println("Enter the value of element: ");
+            array[count] = input.nextInt();
+
+            /* Use nested if and for statements instead of using them separately or using separated methods to perform
+            * that task*/
+            if (NUMBER_OF_ELEMENTS == count+ SHIFTING_VALUE_OF_COUNTER) {
+
+                Arrays.sort(array);
+                for (int value : array) {
+                    System.out.printf("\n%d", value);
+
+                    lowerValue = array[0];
+                    highestValue = array[LAST_INDEX];
+                    System.out.printf("\n\n%d is max, %d is min", highestValue, lowerValue);
+                }
+            }
+        }
+    }
+}
